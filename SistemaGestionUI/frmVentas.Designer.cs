c0ae3ed0ffa2bd58e91cb2f6fadb4940ad240d41
José Rivera <jmrivera82@gmail.com>
2024-02-14 12:02:46 -0300

    formulario ventas

---
+++ SistemaGestionUI/frmVentas.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SistemaGestionUI
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDatos = new DataGridView();
+            btnListar = new Button();
+            btnEliminar = new Button();
+            btnActualizar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Location = new Point(89, 124);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.RowTemplate.Height = 25;
+            dgvDatos.Size = new Size(635, 211);
+            dgvDatos.TabIndex = 0;
+            // 
+            // btnListar
+            // 
+            btnListar.Location = new Point(150, 47);
+            btnListar.Name = "btnListar";
+            btnListar.Size = new Size(75, 23);
+            btnListar.TabIndex = 1;
+            btnListar.Text = "Listar";
+            btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(276, 47);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(75, 23);
+            btnEliminar.TabIndex = 2;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Location = new Point(410, 47);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(75, 23);
+            btnActualizar.TabIndex = 3;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // frmVentas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnActualizar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnListar);
+            Controls.Add(dgvDatos);
+            Name = "frmVentas";
+            Text = "frmVentas";
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvDatos;
+        private Button btnListar;
+        private Button btnEliminar;
+        private Button btnActualizar;
+    }
+}
